6693c2dd60bf11a751ce428bf03c42a9705881c1
lehodiep.1999@gmail.com <lehodiep.1999@gmail.com>
2023-10-20 22:13:25 +0700

    project init

---
+++ src/middlewares/users.middlewares.ts
@@ -0,0 +1,108 @@
+//giả xử là anh đang là route '/login'
+//thì người dùng sẽ truyền email và password
+//tạo 1 req có body là email và password
+//- làm 1 middleware kiểm tra xem email và password có
+
+import { Request, Response, NextFunction } from 'express'
+import { checkSchema } from 'express-validator'
+import usersService from '~/services/users.services'
+import { validate } from '~/utils/validation'
+
+//được truyền lên hay không ?
+export const loginValidator = (req: Request, res: Response, next: NextFunction) => {
+  const { email, password } = req.body
+  if (!email || !password) {
+    return res.status(400).json({
+      error: 'Missing email or password'
+    })
+  }
+  next()
+}
+
+export const registerValidator = validate(
+  checkSchema({
+    name: {
+      notEmpty: true,
+      isString: true,
+      trim: true,
+      isLength: {
+        options: {
+          min: 1,
+          max: 100
+        }
+      }
+    },
+    email: {
+      notEmpty: true,
+      isEmail: true,
+      trim: true,
+      custom: {
+        options: async (value, { req }) => {
+          const isExist = await usersService.checkEmailExist(value)
+          if (isExist) {
+            throw new Error('Email already exists')
+          }
+          return true
+        }
+      }
+    },
+    password: {
+      notEmpty: true,
+      isString: true,
+      isLength: {
+        options: {
+          min: 8,
+          max: 50
+        }
+      },
+      isStrongPassword: {
+        options: {
+          minLength: 8,
+          minLowercase: 1,
+          minUppercase: 1,
+          minNumbers: 1,
+          minSymbols: 1
+        }
+      },
+      errorMessage:
+        'Password must be at least 8 characters long, contain at least 1 lowercase, 1 uppercase, 1 number and 1 symbol'
+    },
+    confirm_password: {
+      notEmpty: true,
+      isString: true,
+      isLength: {
+        options: {
+          min: 8,
+          max: 50
+        }
+      },
+      isStrongPassword: {
+        options: {
+          minLength: 8,
+          minLowercase: 1,
+          minUppercase: 1,
+          minNumbers: 1,
+          minSymbols: 1
+        }
+      },
+      errorMessage:
+        'confirm_password must be at least 8 characters long, contain at least 1 lowercase, 1 uppercase, 1 number and 1 symbol',
+      custom: {
+        options: (value, { req }) => {
+          if (value !== req.body.password) {
+            throw new Error('confirm_password does not match password')
+          }
+          return true
+        }
+      }
+    },
+    date_of_birth: {
+      isISO8601: {
+        options: {
+          strict: true,
+          strictSeparator: true
+        }
+      }
+    }
+  })
+)
