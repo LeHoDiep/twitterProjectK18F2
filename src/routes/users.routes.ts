3fab6c11eed9dc2a5c181bcadcc7890cbe87b70a
lehodiep.1999@gmail.com <lehodiep.1999@gmail.com>
2023-10-31 20:50:17 +0700

    hoan thanh verify email token

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,7 +1,13 @@
 import { Router } from 'express'
-import { loginController, logoutController, registerController } from '~/controllers/users.controllers'
+import {
+  emailVerifyTokenController,
+  loginController,
+  logoutController,
+  registerController
+} from '~/controllers/users.controllers'
 import {
   accessTokenValidator,
+  emailVerifyTokenValidator,
   loginValidator,
   refreshTokenValidator,
   registerValidator
@@ -40,4 +46,19 @@ headers: {Authorization: 'Bearer <access_token>'}
 body: {refresh_token: string}
 */
 usersRoute.post('/logout', accessTokenValidator, refreshTokenValidator, wrapAsync(logoutController))
+
+/*
+des: verify email token
+khi người dùng đăng ký họ sẽ nhận đc mail có link dạng
+http://localhost:3000/users/verify-email?token=<email_verify_token>
+nếu mà em nhấp vào link thì sẽ tạo ra req gữi lên email_verify_token lên server
+server kiểm tra email_verify_token có hợp lệ hay không ?
+thì từ decoded_email_verify_token lấy ra user_id
+và vào user_id đó để update email_verify_token thành '', verify = 1, update_at
+path: /users/verify-email
+method: POST
+body: {email_verify_token: string}
+*/
+usersRoute.post('/verify-email', emailVerifyTokenValidator, wrapAsync(emailVerifyTokenController))
+
 export default usersRoute
--- src/services/users.services.ts
+++ src/services/users.services.ts
@@ -3,7 +3,7 @@ import databaseService from './database.services'
 import { RegisterReqBody } from '~/models/requests/User.requests'
 import { hashPassword } from '~/utils/crypto'
 import { signToken } from '~/utils/jwt'
-import { TokenType } from '~/constants/enums'
+import { TokenType, UserVerifyStatus } from '~/constants/enums'
 import RefreshToken from '~/models/schemas/RefreshToken.schema'
 import { ObjectId } from 'mongodb'
 import { USERS_MESSAGES } from '~/constants/messages'
@@ -13,16 +13,28 @@ class UsersService {
   private signAccessToken(user_id: string) {
     return signToken({
       payload: { user_id, token_type: TokenType.AccessToken },
-      options: { expiresIn: process.env.ACCESS_TOKEN_EXPIRE_IN }
+      options: { expiresIn: process.env.ACCESS_TOKEN_EXPIRE_IN },
+      privateKey: process.env.JWT_SECRET_ACCESS_TOKEN as string
     })
   }
   //hàm nhận vào user_id và bỏ vào payload để tạo refresh_token
   private signRefreshToken(user_id: string) {
     return signToken({
       payload: { user_id, token_type: TokenType.RefreshToken },
-      options: { expiresIn: process.env.REFRESH_TOKEN_EXPIRE_IN }
+      options: { expiresIn: process.env.REFRESH_TOKEN_EXPIRE_IN },
+      privateKey: process.env.JWT_SECRET_REFRESH_TOKEN as string
     })
   }
+
+  //hàm signEmailVerifyToken
+  private signEmailVerifyToken(user_id: string) {
+    return signToken({
+      payload: { user_id, token_type: TokenType.EmailVerificationToken },
+      options: { expiresIn: process.env.EMAIL_VERIFY_TOKEN_EXPIRE_IN },
+      privateKey: process.env.JWT_SECRET_EMAIL_VERIFY_TOKEN as string
+    })
+  }
+
   // ký asccess_token và refresh_token
   private signAccessAndRefreshToken(user_id: string) {
     return Promise.all([this.signAccessToken(user_id), this.signRefreshToken(user_id)])
@@ -34,15 +46,19 @@ class UsersService {
   }
 
   async register(payload: RegisterReqBody) {
+    const user_id = new ObjectId()
+    const email_verify_token = await this.signEmailVerifyToken(user_id.toString())
     const result = await databaseService.users.insertOne(
       new User({
         ...payload,
+        _id: user_id,
+        email_verify_token,
         date_of_birth: new Date(payload.date_of_birth),
         password: hashPassword(payload.password)
       })
     )
-    const user_id = result.insertedId.toString()
-    const [access_token, refresh_token] = await this.signAccessAndRefreshToken(user_id)
+
+    const [access_token, refresh_token] = await this.signAccessAndRefreshToken(user_id.toString())
     //lưu refresh_token vào db
     await databaseService.refreshTokens.insertOne(
       new RefreshToken({
@@ -50,6 +66,9 @@ class UsersService {
         user_id: new ObjectId(user_id)
       })
     )
+
+    //giả lập gữi mail
+    console.log(email_verify_token)
     return { access_token, refresh_token }
   }
 
@@ -68,6 +87,29 @@ class UsersService {
   async logout(refresh_token: string) {
     await databaseService.refreshTokens.deleteOne({ token: refresh_token })
     return { message: USERS_MESSAGES.LOGOUT_SUCCESS }
+  }
+
+  async verifyEmail(user_id: string) {
+    //update lại user
+    await databaseService.users.updateOne({ _id: new ObjectId(user_id) }, [
+      {
+        $set: {
+          verify: UserVerifyStatus.Verified,
+          email_verify_token: '',
+          updated_at: '$$NOW'
+        }
+      }
+    ])
+    //tạo ra access_token và refresh_token
+    const [access_token, refresh_token] = await this.signAccessAndRefreshToken(user_id)
+    //lưu refresh_token vào db
+    await databaseService.refreshTokens.insertOne(
+      new RefreshToken({
+        token: refresh_token,
+        user_id: new ObjectId(user_id)
+      })
+    )
+    return { access_token, refresh_token }
   }
 }
 
